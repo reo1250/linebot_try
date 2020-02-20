f6945eb47d207c03e16885c6639c9a8cebdf5540
reo1250 <suenaga.reo667@mail.kyutech.jp>
2020-02-20 12:55:44 +0900

    delete import random

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
-import random
 
 def myIndex(1,x):
 	if x in 1:

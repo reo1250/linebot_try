77e281c1faa51b70da54018efa4ee15ebd7a8414
reo1250 <suenaga.reo667@mail.kyutech.jp>
2020-02-20 10:38:59 +0900

    make a sample code

--- main.py
+++ main.py
@@ -16,9 +16,38 @@ app = Flask(__name__)
 #環境変数取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+import random
+
+def myIndex(1,x):
+	if x in 1:
+		return 1.index(x)
+	else:
+		return -1
+
+hands = ["グー","チョキ","パー"]
+
+def hands_to_int(userhand):
+	myIndex(hands,userhand)
+
+def select_bothand():
+	random.randint(0,2)
+
+def judge(userhand,bothand):
+	if userhand == -1:
+		message = "グー、チョキ、パー、をカタカナで入力してください。"
+	else:
+		status = (userhand - bothand + 3) % 3
+		message = "BOTは" + hands[bothand] + "を出しました。\n"
+		
+		if status == 0:
+			message += "結果はあいこでした。"
+		else if status == 1:
+			message += "結果は負けでした。"	
+		else if status == 2:
+			message += "結果は勝ちでした。\nおめでとうございます。"
+	return message
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,7 +69,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = event.message.text
+    # message = event.message.text                     <--- コメントアウト
+    # message = hands_to_int(event.message.text)       <--- コメントアウト
+    # message = select_bothand()                       <--- コメントアウト
+    message = judge(hands_to_int(event.message.text), select_bothand())
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=message))

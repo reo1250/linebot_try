d3eb5c3a57adc94da3363fb523b8db9ad9c55c72
reo1250 <suenaga.reo667@mail.kyutech.jp>
2020-02-20 17:31:35 +0900

    use arry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, QuickReplyButton, MessageAction, QuickReply, TextSendMessage,
 )
 import os
 import random
@@ -78,75 +78,30 @@ def callback():
 	return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
+if myIndex(l,x) == -1:
+	def response_message(event):
+		hands_list = ["グー", "チョキ", "パー"]
 
+		items = [QuickReplyButton(action=MessageAction(label=f"{hand}", text=f"{hand}")) for hand in hands_list]
 
-def handle_message(event):
-	# message = event.message.text                     <--- コメントアウト
-	# message = hands_to_int(event.message.text)       <--- コメントアウト
-	# message = select_bothand()                       <--- コメントアウト
-	message = judge(hands_to_int(event.message.text), select_bothand())
-	line_bot_api.reply_message(
-		event.reply_token,
-		#TextSendMessage(text=message)
-		createMessages(message)
-	)
+		messages = TextSendMessage(text="グー、チョキ、パーのうちどれを出しますか?",quick_reply=QuickReply(items=items))
+
+		line_bot_api.reply_message(event.reply_token, messages=messages)
+
+elif:
+	def handle_message(event):
+		# message = event.message.text                     <--- コメントアウト
+		# message = hands_to_int(event.message.text)       <--- コメントアウト
+		# message = select_bothand()                       <--- コメントアウト
+		message = judge(hands_to_int(event.message.text), select_bothand())
+		line_bot_api.reply_message(
+			event.reply_token,
+			#TextSendMessage(text=message)
+			createMessages(message)
+		)
 
 
 if __name__ == "__main__":
 #    app.run()
 	port = int(os.getenv("PORT", 5000))
 	app.run(host="0.0.0.0", port=port)
-"""
-
-import os
-
-from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, QuickReplyButton, MessageAction, QuickReply, TextSendMessage)
-
-app = Flask(__name__)
-
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def response_message(event):
-    hands_list = ["グー", "チョキ", "パー"]
-
-    items = [QuickReplyButton(action=MessageAction(label=f"{hand}", text=f"{hand}")) for hand in hands_list]
-
-    messages = TextSendMessage(text="グー、チョキ、パーのうちどれを出しますか?",
-                               quick_reply=QuickReply(items=items))
-
-    line_bot_api.reply_message(event.reply_token, messages=messages)
-
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-#"""
---
+++ sub.py
@@ -0,0 +1,52 @@
+
+import os
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, QuickReplyButton, MessageAction, QuickReply, TextSendMessage)
+
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def response_message(event):
+    hands_list = ["グー", "チョキ", "パー"]
+
+    items = [QuickReplyButton(action=MessageAction(label=f"{hand}", text=f"{hand}")) for hand in hands_list]
+
+    messages = TextSendMessage(text="グー、チョキ、パーのうちどれを出しますか?",
+                               quick_reply=QuickReply(items=items))
+
+    line_bot_api.reply_message(event.reply_token, messages=messages)
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

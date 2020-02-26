918a4a3649d76190d19d1898cc511ea3f79cdbff
reo1250 <suenaga.reo667@mail.kyutech.jp>
2020-02-26 13:36:07 +0900

    tell datetime

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	
-	message = datetime.fromtimestamp(int(float(event.timestamp)))
+	message = datetime.fromtimestamp(event.timestamp)
 	line_bot_api.reply_message(
 		event.reply_token,
 		TextSendMessage(text=message))		
